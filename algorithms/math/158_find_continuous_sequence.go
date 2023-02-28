063fc33578ecde6e2a5bb4792e1612ab8f4adb5c
luo jun <luojun96@live.cn>
2023-02-28 18:48:42 +0800

    Add a algorithm problem with double points solution.

---
+++ algorithms/math/158_find_continuous_sequence.go
@@ -0,0 +1,24 @@
+package main
+
+// https://leetcode.cn/problems/he-wei-sde-lian-xu-zheng-shu-xu-lie-lcof/solutions/
+// input: target = 9
+// output: [[2,3,4],[4,5]]
+func findContinuousSequence(target int) [][]int {
+	res := make([][]int, 0)
+	for l, r := 1, 2; l < r; {
+		sum := ((l + r) * (r - l + 1)) >> 1
+		if sum == target {
+			arr := make([]int, r-l+1)
+			for i := l; i <= r; i++ {
+				arr[i-l] = i
+			}
+			res = append(res, arr)
+			l++
+		} else if sum < target {
+			r++
+		} else {
+			l++
+		}
+	}
+	return res
+}
