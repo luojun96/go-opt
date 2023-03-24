b9924364e64f89fc757571200375050cce492824
Jun Luo <luojun96@live.cn>
2023-03-24 11:40:18 +0800

    add a algorithm problem about hash.

---
+++ algorithms/array/171_single_number.go
@@ -0,0 +1,15 @@
+package main
+
+// https://leetcode.cn/problems/WGki4K/description/
+func singleNumber(nums []int) int {
+	m := map[int]int{}
+	for _, num := range nums {
+		m[num]++
+	}
+	for num, count := range m {
+		if count == 1 {
+			return num
+		}
+	}
+	return 0
+}
