e92de7addd5f22169387a4514190a87078ac3ce5
Jun Luo <luojun96@live.cn>
2023-03-25 22:38:57 +0800

    add a binary search tree problem.

---
+++ algorithms/tree/175_kth_smallest.go
@@ -0,0 +1,19 @@
+package main
+
+// https://leetcode.cn/problems/kth-smallest-element-in-a-bst/solutions/
+func kthSmallest(root *TreeNode, k int) int {
+	stack := []*TreeNode{}
+	for {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		k--
+		if k == 0 {
+			return root.Val
+		}
+		root = root.Right
+	}
+}
