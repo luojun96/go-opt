235b8078ac0bfaf9b5e00623fbdc82a5f59b70f4
Jun Luo <luojun96@live.cn>
2023-04-02 00:25:58 +0800

    add linked list problem.

---
+++ algorithms/linkedlist/176_reorder_list.go
@@ -0,0 +1,23 @@
+package main
+
+// https://leetcode.cn/problems/LGjMqU/
+func reorderList(head *ListNode) {
+	if head == nil {
+		return
+	}
+	nodes := []*ListNode{}
+	for node := head; node != nil; node = node.Next {
+		nodes = append(nodes, node)
+	}
+	i, j := 0, len(nodes)-1
+	for i < j {
+		nodes[i].Next = nodes[j]
+		i++
+		if i == j {
+			break
+		}
+		nodes[j].Next = nodes[i]
+		j--
+	}
+	nodes[i].Next = nil
+}
