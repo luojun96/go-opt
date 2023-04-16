713492745fd7fa1452de41bdfaacea13870de1a2
Jun Luo <luojun96@live.cn>
2023-04-16 22:02:44 +0800

    fix vet issues in algorithms/sort.

--- algorithms/sort/115_find_kth_largest_number.go
+++
@@ -1,8 +0,0 @@
-package sort
-
-// https://leetcode.cn/problems/find-the-kth-largest-integer-in-the-array/
-// input: nums = ["3","6","7","10"], k = 4
-// output: "3"
-func kthLargestNumber(nums []string, k int) string {
-
-}
