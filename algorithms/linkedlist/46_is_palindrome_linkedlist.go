709f9480604297144700b86fd328ad9cd7e79cbf
Jun Luo <luojun96@live.cn>
2023-04-17 22:21:36 +0800

    fix issue.

--- algorithms/linkedlist/46_is_palindrome_linkedlist.go
+++ algorithms/linkedlist/46_is_palindrome_linkedlist.go
@@ -3,6 +3,42 @@ package linkedlist
 // https://leetcode.cn/problems/palindrome-linked-list/
 // input: head = [1,2,2,1]
 // output: true
+func checkIsPalindrome(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+
+	if fast != nil {
+		slow = slow.Next
+	}
+
+	left := head
+	right := reverseLinkedlist(slow)
+
+	for right != nil {
+		if left.Val != right.Val {
+			return false
+		}
+		left = left.Next
+		right = right.Next
+	}
+	return true
+}
+
+func reverseLinkedlist(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+}
+
 func check_isPalindrome(head *ListNode) bool {
 	slow, fast := head, head
 	for fast != nil && fast.Next != nil {
