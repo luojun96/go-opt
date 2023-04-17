5f5e4eebb154db21e7121995bf9371bd6508752c
Jun Luo <luojun96@live.cn>
2023-04-17 22:21:36 +0800

    fix issue in linkedlist.

--- algorithms/linkedlist/46_is_palindrome_linkedlist.go
+++ algorithms/linkedlist/46_is_palindrome_linkedlist.go
@@ -38,38 +38,3 @@ func reverseLinkedlist(head *ListNode) *ListNode {
 	}
 	return prev
 }
-
-func check_isPalindrome(head *ListNode) bool {
-	slow, fast := head, head
-	for fast != nil && fast.Next != nil {
-		slow = slow.Next
-		fast = fast.Next.Next
-	}
-
-	if fast != nil {
-		slow = slow.Next
-	}
-
-	left := head
-	right := reverse_linkedlist(slow)
-
-	for right != nil {
-		if left.Val != right.Val {
-			return false
-		}
-		left = left.Next
-		right = right.Next
-	}
-	return true
-}
-
-func reverse_linkedlist(head *ListNode) *ListNode {
-	prev, curr := head, head
-	for curr != nil {
-		next := curr.Next
-		curr.Next = prev
-		prev = curr
-		curr = next
-	}
-	return prev
-}
