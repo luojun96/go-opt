efb5ca1f334334e67f1110d19287ec43a73d29fd
Jun Luo <luojun96@live.cn>
2023-04-25 22:38:29 +0800

    update concurrency/cond.

--- concurrency/cond/main.go
+++ concurrency/cond/main.go
@@ -1,32 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
 func main() {
-// 	c := sync.NewCond(&sync.Mutex{})
-// 
-// 	var ready int
-// 
-// 	for i := 0; i < 10; i++ {
-// 		go func(i int) {
-// 			time.Sleep(time.Duration(rand.Int63n(10)) * time.Second)
-// 
-// 			c.L.Lock()
-// 			ready++
-// 			c.L.Unlock()
-// 
-// 			fmt.Printf("Athletes #%d is ready.\n", i)
-// 			
-// 			c.Broadcast()
-// 		}(i)
-// 	}
-// 
-// 	c.L.Lock()
-// 	for ready != 10 {
-// 		c.Wait()
-// 		fmt.Println("Judge is called once.")
-// 	}
-// 	c.L.Unlock()
-// 
-// 	fmt.Println("All athletes are ready now,  kick off...")
-// 
-	Run()
+	executeWithCondtion()
+	// Run()
+}
+
+func executeWithCondtion() {
+	c := sync.NewCond(&sync.Mutex{})
+	var ready int
+	for i := 0; i < 10; i++ {
+		go func(i int) {
+			time.Sleep(time.Duration(rand.Int63n(10)) * time.Second)
+			c.L.Lock()
+			ready++
+			c.L.Unlock()
+			fmt.Printf("Athletes #%d is ready.\n", i)
+			c.Broadcast()
+		}(i)
+	}
+
+	c.L.Lock()
+	for ready != 10 {
+		fmt.Println("Wait...")
+		c.Wait()
+	}
+	c.L.Unlock()
+	fmt.Println("All athletes are ready now,  kick off...")
 }
