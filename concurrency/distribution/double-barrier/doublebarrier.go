b1c81a757babe26aa1e810fd356065b1a750ad26
Jun Luo <luojun96@live.cn>
2023-04-27 19:25:31 +0800

    fix issues in distribution double barrier using etcd.

--- concurrency/distribution/double-barrier/doublebarrier.go
+++ concurrency/distribution/double-barrier/doublebarrier.go
@@ -4,34 +4,47 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"os"
 	"strings"
+	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.etcd.io/etcd/client/v3/concurrency"
 	recipe "go.etcd.io/etcd/client/v3/experimental/recipes"
 )
 
 var (
-	addr        = flag.String("addr", "https://192.168.31.23:2379", "etcd address")
+	addr        = flag.String("addr", "http://127.0.0.1:2379", "etcd address")
 	barrierName = flag.String("name", "my-test-doublebarrier", "barrier name")
 	count       = flag.Int("c", 2, "")
 )
 
-func exec() {
+func main() {
 	flag.Parse()
 
 	endpoints := strings.Split(*addr, ",")
 
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints: endpoints,
+		Endpoints:   endpoints,
+		DialTimeout: 5 * time.Second,
 	})
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
 	}
 	defer cli.Close()
 
+	fmt.Println("connected to etcd")
+
+	// create a session
+	s1, err := concurrency.NewSession(cli)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer s1.Close()
+	fmt.Println("created session")
+
 	b := recipe.NewDoubleBarrier(s1, *barrierName, *count)
+	fmt.Println("created double barrier")
 
 	consolescanner := bufio.NewScanner(os.Stdin)
 	for consolescanner.Scan() {
--- go.mod
+++ go.mod
@@ -3,20 +3,24 @@ module github.com/luojun96/go-opt
 go 1.19
 
 require (
+	github.com/mdlayher/schedgroup v1.0.0
+	github.com/pkg/errors v0.8.1
+	github.com/vardius/gollback v1.1.1
+	go.etcd.io/etcd/client/v3 v3.5.8
+	golang.org/x/sync v0.1.0
+)
+
+require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/mdlayher/schedgroup v1.0.0 // indirect
-	github.com/vardius/gollback v1.1.1 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.8 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.8 // indirect
-	go.etcd.io/etcd/client/v3 v3.5.8 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
 	golang.org/x/text v0.3.5 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
