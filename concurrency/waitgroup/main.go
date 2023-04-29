68c150c06054f3833f265d88cb3cb62a1226f185
Jun Luo <luojun96@live.cn>
2023-04-29 14:42:34 +0800

    update sync waitgroup.

--- concurrency/waitgroup/main.go
+++ concurrency/waitgroup/main.go
@@ -3,42 +3,26 @@ package main
 import (
 	"fmt"
 	"sync"
-	"time"
 )
 
-type Counter struct {
-	mu    sync.Mutex
-	count uint64
-}
-
-func (c *Counter) Incr() {
-	c.mu.Lock()
-	c.count++
-	c.mu.Unlock()
-}
-
-func (c *Counter) Count() uint64 {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.count
-}
-
-func worker(c *Counter, wg *sync.WaitGroup) {
-	defer wg.Done()
-	time.Sleep(time.Second)
-	c.Incr()
-}
-
 func main() {
-	var counter Counter
-	var wg sync.WaitGroup
-	wg.Add(100)
+	var mu sync.Mutex
 
-	for i := 0; i < 100; i++ {
-		go worker(&counter, &wg)
+	var count = 0
+	var wg sync.WaitGroup
+	wg.Add(10)
+
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 10000; j++ {
+				mu.Lock()
+				count++
+				mu.Unlock()
+			}
+		}()
 	}
 
 	wg.Wait()
-
-	fmt.Println(counter.Count())
+	fmt.Println(count)
 }
