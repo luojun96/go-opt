9b5a4cb35b0a7ba0ede10d81c9d9e83f2ca0c1ad
Jun Luo <luojun96@live.cn>
2023-04-30 23:38:14 +0800

    update decorator.

--- ease/decorator/timedsum_test.go
+++ ease/decorator/timedsum_test.go
@@ -9,5 +9,5 @@ func TestTimedSum(t *testing.T) {
 	sum1 := timedSumFunc(Sum1)
 	sum2 := timedSumFunc(Sum2)
 
-	fmt.Printf("%d, %d\n", sum1(-10000, 1000000), sum2(-10000, 100000:))
+	fmt.Printf("%d, %d\n", sum1(-10000, 1000000), sum2(-10000, 100000))
 }
