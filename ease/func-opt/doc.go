1b531b9b41f6ce3db1e61d3bd068f164baf2ea8d
Jun Luo <luojun96@live.cn>
2023-05-19 15:59:04 +0800

    fix format issue

--- ease/func-opt/doc.go
+++ ease/func-opt/doc.go
@@ -1,8 +1,8 @@
 // Functional options is a pattern in which you declare an opaque Option type
 // that records information in some internal struct. You accept a variadic number
-//
 // of these Options and act upon the full information recorder by the options
 // on the internal struct.
+// 
 // https://commandcenter.blogspot.com/2014/01/self-referential-functions-and-design.html
 // https://dave.cheney.net/2014/10/17/functional-options-for-friendly-apis:w
 // https://github.com/uber-go/guide/blob/master/style.md#functional-options
