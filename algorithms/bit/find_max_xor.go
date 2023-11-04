67423e83124445357873cba048e2478eb2b7448b
Jun Luo <luojun96@live.cn>
2023-11-04 10:59:44 +0800

    update

---
+++ algorithms/bit/find_max_xor.go
@@ -0,0 +1,29 @@
+package bit
+
+const highBit = 30
+
+func findMaximumXOR(nums []int) (x int) {
+	for k := highBit; k >= 0; k-- {
+		seen := map[int]bool{}
+		for _, num := range nums {
+			seen[num>>k] = true
+		}
+		xNext := x*2 + 1
+		found := false
+
+		for _, num := range nums {
+			if seen[num>>k^xNext] {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			x = xNext
+		} else {
+			x = xNext - 1
+		}
+	}
+
+	return
+}
