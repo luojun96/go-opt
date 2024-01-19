750c8851f2e8c36a7915ed4c4e5bb794b07bccc1
Jun Luo <luojun96@live.cn>
2024-01-19 17:53:28 +0800

    update

--- algorithms/string/23_reverse_string.go
+++ algorithms/string/23_reverse_string.go
@@ -4,8 +4,8 @@ package string
 // input: s = ["h","e","l","l","o"]
 // output: ["o","l","l","e","h"]
 func reverseString(s []byte) {
-	for left, right := 0, len(s) - 1; left < right; left++ {
-		s[left], s[right] = s[right], s[left]
-		right--
+	for l, r := 0, len(s)-1; l < r; l++ {
+		s[l], s[r] = s[r], s[l]
+		r--
 	}
 }
---
+++ algorithms/string/23_reverse_string_test.go
@@ -0,0 +1,34 @@
+package string
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_reverseString(t *testing.T) {
+	type args struct {
+		s []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "case1",
+			args: args{
+				s: []byte{'o', 'l', 'l', 'e', 'h'},
+			},
+			want: []byte{'h', 'e', 'l', 'l', 'o'},
+		},
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			reverseString(tt.args.s)
+			if !reflect.DeepEqual(tt.args.s, tt.want) {
+				t.Errorf("reverseString() = %v, want %v", tt.args.s, tt.want)
+			}
+		})
+	}
+}
